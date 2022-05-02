6da5984e0599a7afdd429ad2563ccb1944846684
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-05-02 17:31:41 +0200

    Add user

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!-- Dove faccio l'import dei file che mi servono -->
 <?php
-include_once __DIR__  . '/products/product.php';
+// include_once __DIR__  . '/products/product.php';
 include_once __DIR__  . '/products/food.php';
 include_once __DIR__  . '/products/accessories.php';
 include_once __DIR__  . '/products/healthCare.php';
+
+include_once __DIR__  . '/users/user.php';
 
     $prodotto1 = new Food('croccchette', 16.99, 'cane', 'secco');
     $prodotto1->setBrand('monge');
     // $prodotto1->setAge('junior');
     var_dump($prodotto1);
+
+    $utente1 = new User('Franca', 'Cocco', true);
+    var_dump($utente1);
---
+++ users/user.php
@@ -0,0 +1,19 @@
+<!-- Qui definisco un qualsiasi tipo di utente -->
+<?php
+    class User {
+        private $name;
+        private $surname;
+        private $discount = 0;
+        private $validCc;
+
+        public function __construct($_name, $_surname, $_validCc)
+        {
+            $this->name = $_name;
+            $this->surname = $_surname;
+            $this->validCc = $_validCc;
+        }
+        
+    }
+
+    
+?>
